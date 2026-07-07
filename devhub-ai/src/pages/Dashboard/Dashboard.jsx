8d4512fb21bb33abb817314a85542d57d49f288b
Irfan762 <irfantamboli762@gmail.com>
2026-07-07 23:49:40 +0530

    Add Card compnenetn

--- devhub-ai/src/pages/Dashboard/Dashboard.jsx
+++ devhub-ai/src/pages/Dashboard/Dashboard.jsx
@@ -1,29 +1,51 @@
-import Navbar from "../../components/Navbar/Navbar"
-import Sidebar from "../../components/Sidebar/Sidebar"
-import Card from '../../components/Card/Card'
+import Navbar from "../../components/Navbar/Navbar";
+import Sidebar from "../../components/Sidebar/Sidebar";
+import Card from "../../components/Card/Card";
 
-function Dashboard(){
-    return(
-        <>
-        <Navbar/>
-        <div>
-            <Sidebar/>
-            <main className="flex-1 p-6">
-                <h1 className="text-3xl font-bold">Dashboard</h1>
+function Dashboard() {
 
-                <div className="grid grid-cols-3 gap-6 mt-6">
-                    <Card title="post" value="25"/>
-                    <Card title="Project" value="10"/>
-                    <Card title="Messages" value="8"/>
+  const dashboardData = [
+    {
+      title: "Posts",
+      value: 25,
+    },
+    {
+      title: "Projects",
+      value: 10,
+    },
+    {
+      title: "Messages",
+      value: 8,
+    },
+    {
+      title: "AI Reviews",
+      value: 16,
+    },
+  ];
 
+  return (
+    <>
+      <Navbar />
 
-                </div>
-            </main>
-        </div>
+      <div className="flex">
+        <Sidebar />
 
+        <main className="flex-1 p-6">
+          <h1 className="text-3xl font-bold">Dashboard</h1>
 
-        </>
-    );
+          <div className="grid grid-cols-4 gap-6 mt-6">
+            {dashboardData.map((item, index) => (
+              <Card
+                key={index}
+                title={item.title}
+                value={item.value}
+              />
+            ))}
+          </div>
+        </main>
+      </div>
+    </>
+  );
 }
 
 export default Dashboard;
